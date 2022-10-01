332b260dd4351eb59bb28228ae7416413749fc38
Ana <9025015386@gmail.ru>
2022-10-01 13:15:42 +0500

    Задание 3

---
+++ src/Auto.java
@@ -0,0 +1,26 @@
+class Auto {
+    String mark;
+    String model;
+    Double engineCapacityInLiters;
+    String color;
+    int yearOfProduction;
+    String countryOfAssembly;
+
+    public Auto(String mark, String model, Double engineCapacityInLiters, String color, int yearOfProduction, String countryOfAssembly) {
+        this.mark = mark;
+        this.model = model;
+        this.engineCapacityInLiters = engineCapacityInLiters;
+        this.color = color;
+        this.yearOfProduction = yearOfProduction;
+        this.countryOfAssembly = countryOfAssembly;
+    }
+    void cars(){
+System.out.println("Марка машины - " + mark);
+System.out.println("Модель - " + model);
+System.out.println("Объем двигателя - " + engineCapacityInLiters + " литров");
+System.out.println("Цвет кузова - " + color);
+System.out.println("Год производства - " + yearOfProduction);
+System.out.println("Страна производства - " + countryOfAssembly);
+System.out.println("   ");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+Auto lada = new Auto("Lada", "Grande", 1.7, "Желтый", 2015, "Россия");
+lada.cars();
+Auto audi = new Auto("Audi", "A8 50 L TDI quattro", 3.0, "Черный", 2020, "Германия");
+audi.cars();
+Auto bmw = new Auto("BMW", "Z8", 3.0, "Черный", 2021, "Германия");
+bmw.cars();
+Auto kia = new Auto("Kia", "Sportage 4 поколение", 2.4, "Красный", 2018, "Южная Корея");
+kia.cars();
+Auto hyundai = new Auto("Hyundai", "Avante", 1.6, "Оранжевый",2016, "Южная Корея");
+hyundai.cars();
     }
 }
